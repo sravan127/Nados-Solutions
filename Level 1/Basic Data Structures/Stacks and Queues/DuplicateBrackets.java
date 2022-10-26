9149e10f0d8d1b5a35a22e668760205738d529df
saisravan127 <saisravanab@epam.com>
2022-10-26 10:03:00 +0530

    Added Duplicate Brackets - 26Oct2022

---
+++ Level 1/Basic Data Structures/Stacks and Queues/DuplicateBrackets.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class DuplicateBrackets {
+    public static boolean checkBalanced(String str){
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<str.length();i++){
+           if(str.charAt(i)==')'){
+              if(st.peek()=='(')
+                 return true;
+              else{
+                 while(st.peek()!='('){
+                    st.pop();
+                 }
+                 st.pop();
+              }
+           }
+           else{
+              st.push(str.charAt(i));
+           }
+        }
+        return false;
+     }
+     public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+         System.out.println(checkBalanced(str));
+     }
+}
