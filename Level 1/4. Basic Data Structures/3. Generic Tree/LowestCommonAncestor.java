179dea9910a985219dfb7f5f8fcf8cbc7e726057
saisravan127 <saisravanab@epam.com>
2022-11-01 12:28:56 +0530

    Added LCA in Generic Tree - 01Nov2022

---
+++ Level 1/4. Basic Data Structures/3. Generic Tree/LowestCommonAncestor.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+
+public class LowestCommonAncestor {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  public static ArrayList<Integer> nodeToRootPath(Node node, int data) {
+    if (node.data == data) {
+      ArrayList<Integer> path = new ArrayList<>();
+      path.add(node.data);
+      return path;
+    }
+
+    for (Node child : node.children) {
+      ArrayList<Integer> ptc = nodeToRootPath(child, data);
+      if (ptc.size() > 0) {
+        ptc.add(node.data);
+        return ptc;
+      }
+    }
+
+    return new ArrayList<>();
+  }
+
+  public static int lca(Node node, int d1, int d2) {
+   ArrayList<Integer> d1list=nodeToRootPath(node,d1);
+   ArrayList<Integer> d2list=nodeToRootPath(node,d2);
+  
+   int i,j;
+   for(i=d1list.size()-1,j=d2list.size()-1;i>=0&&j>=0;){
+     if(d1list.get(i)==d2list.get(j)){
+       i--;j--;
+     }
+     else break;
+   }
+   if(i<0)return d2list.get(j+1);
+   else return d1list.get(i+1);
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    int d1 = Integer.parseInt(br.readLine());
+    int d2 = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    int lca = lca(root, d1, d2);
+    System.out.println(lca);
+    // display(root);
+  }
+
+}
