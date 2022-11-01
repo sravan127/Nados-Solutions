3be9448e1c60ee86567fb9eca032c27139f080dc
saisravan127 <saisravanab@epam.com>
2022-11-01 11:33:26 +0530

    Added Linearize a Generic Tree - 01Nov2022

---
+++ Level 1/4. Basic Data Structures/3. Generic Tree/LinearizeGT.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class LinearizeGT {
+  static Queue<Node> q=new ArrayDeque<>();
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+  public static void traverse(Node node){
+    q.add(node);
+    for(Node child: node.children){
+      traverse(child);
+    }
+  }
+  public static void linearize(Node node){
+    traverse(node);
+    while(q.size()>0){
+      Node nd=q.remove();
+      nd.children.clear();
+      if(q.size()!=0)
+        nd.children.add(q.peek());
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    Node root = construct(arr);
+    linearize(root);
+    display(root);
+  }
+
+}
