38acaa643dfbac833e4d6e5cc218656f66ff44a0
saisravan127 <saisravanab@epam.com>
2022-12-31 12:33:03 +0530

    Added 0/1 Knapsack - 31Dec2022

---
+++ Level 1/3. Dynamic Programming/1. Dynamic Programming and Greedy/ZeroOneKnapsack.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class ZeroOneKnapsack {
+
+    public static void main(String[] args) throws Exception {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+
+        int[] val=new int[n];
+        for(int i=0;i<n;i++)
+            val[i]=s.nextInt();
+        
+        int[] wt=new int[n];
+        for(int i=0;i<n;i++)
+            wt[i]=s.nextInt();
+        
+        int capacity=s.nextInt();
+
+        int[][] dp=new int[n+1][capacity+1];
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<capacity+1;j++){
+                if(j<wt[i-1])
+                    dp[i][j]=dp[i-1][j];
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],val[i-1]+dp[i-1][j-wt[i-1]]);
+            }
+        }
+
+        System.out.println(dp[n][capacity]);
+    }
+}
